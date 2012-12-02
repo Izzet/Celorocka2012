951b1ed09b530a5b9e392fea5ab15f3da959cd4a
Izzet <izzetStMr@gmail.com>
2012-12-02 17:27:31 +0100

    Síť hexagonů
    Je hotové vykreslování síťe hexagonů různých typů, přidělování typů
    zatím náhodné

--- assets/engine/geometries.js
+++ assets/engine/geometries.js
@@ -1,7 +1,6 @@
 function Hexagon(position,a,options){
 	GUIObject.call(this);
 	
-	this.color = options.color;
 	this.position = position;
 	this.a = a;
 	this.v = Math.sqrt(3)*a/2;
@@ -13,7 +12,23 @@ function Hexagon(position,a,options){
 	this.boundingCircle = this.children[0];
 	this.boundingCircle.renderable = this.boundingRenderable;
 	
+	//Zde se vyhodnotí vlastnosti dle typu
+	this.type = options.type === undefined ? "plains" : options.type;
+	switch (this.type){
+		case "plains":
+			this.color="#FFFAB3";
+			break;
+		case "swamps":
+			this.color="#575323";
+			break;
+		case "forest":
+			this.color="#398A19";
+			break;
+	}
+	this.riveredSides = 0; //To znamená žádná
+	
 	this.render = function (ctx){
+		
 		var x = this.position.x;
 		var y = this.position.y;
 		
@@ -27,8 +42,11 @@ function Hexagon(position,a,options){
 		ctx.lineTo((x-this.a/2),y+this.v);
 		ctx.lineTo((x-this.a),y);
 		ctx.fillStyle = this.color;
-		ctx.stroke();
+		ctx.fill();
 		ctx.closePath();
+		if(this.riveredSides !== 0){
+			this.riverRender(ctx);
+		}
 	};
 };
 Hexagon.prototype = new GUIObject();
@@ -52,3 +70,39 @@ function Circle(position,r,options){
 	};
 };
 Circle.prototype = new GUIObject();
+
+function HexagonField(positionOfFirst,inRow,rows,a,optionsOfHexagons,oddIsFirst){
+	GUIObject.call(this);
+	
+	this.position = positionOfFirst;
+	
+	if(oddIsFirst){//Podmínka určující, zda budou ve předu liché či sudé řady
+		var delimiter = -1;
+	}
+	else{
+		var delimiter = 1;
+	}
+	
+	var v = Math.sqrt(3)*a/2;
+	var poleTypu = ["plains","forest","swamps"];
+	
+	this.rows = [];
+	for(var i = 0; i < rows;i++){
+		var row = [];
+		if(i%2 === 1){//Podmínka zajišťující posun řad
+			var prefix = delimiter*v;
+		}
+		else{
+			var prefix = 0;
+		}
+		for(var j = 0;j < inRow;j++){
+			this.add(new Hexagon(
+				new Vector2(i*3/2*a,j*2*v+prefix),
+				a,
+				{type:poleTypu[Math.round(Math.random()*3)]}
+			));
+		};
+		this.rows.push(row);
+	};
+};
+HexagonField.prototype = new GUIObject();
--- assets/engine/gui.js
+++ assets/engine/gui.js
@@ -1,7 +1,20 @@
 function GUI(){
 	GUIObject.call(this);
 	
-	this.add(new Hexagon(new Vector2(200,200),50,{color:"#000000"}));
+	this.add(new HexagonField(
+		new Vector2(60,50),//Pozice prvního
+		6, //V jedné řadě je n šestiúhelníků
+		10, //Celkem je n řad
+		50, //Délka strany šestiúhelníku je n
+		{type: "swamps"}, //options šestiúhelníku
+		false // Ve předu jsou liché řady
+	));
+	
+	this.makeRivers = function (hexfield){
+		var start = [Math.ceil(Math.random()*10),Math.ceil(Math.random()*6)]; //[rada;poradi]
+	};
+	
+	this.makeRivers(this.children[0]);
 	
 	this.render = function(ctx){
 		this.renderChildren(ctx);
